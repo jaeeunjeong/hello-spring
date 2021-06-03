95328fcff0c3177b43c63b4c5609eec286eb9f2b
jaeeunjeong <jejeong000@gmail.com>
2021-06-03 22:32:19 +0900

    스프링 데이터 JPA로 변경

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -4,6 +4,7 @@ import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,25 +14,31 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
     //Service, Repository, Autowired없이도 작동 가능.
+    /*
     private final DataSource dataSource;
     private final EntityManager em;
 
     public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
         this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
+
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
 // return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
         return new JpaMemberRepository(em);
-    }
+    }*/
 
 }
